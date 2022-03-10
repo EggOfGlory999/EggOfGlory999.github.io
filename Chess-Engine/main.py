7bc509f4bfda2b6e9689ec2c85e6e75e60793916
EggOfGlory999 <70840786+EggOfGlory999@users.noreply.github.com>
2022-03-09 20:47:02 -0800

    Update main.py

--- Chess-Engine/main.py
+++ Chess-Engine/main.py
@@ -15,18 +15,7 @@ IMAGES = {}
 Initialize a global dictionary of images. This will be called once within the main
 '''
 def_load_images():
-  IMAGES["wP"] - p.image.load("3409_white-pawn.png")
-  IMAGES["bP"] - p.image.load("3403_black-pawn.png")
-  IMAGES["wN"] - p.image.load("3408_white-knight.png")
-  IMAGES["bN"] - p.image.load("3402_black-knight.png")
-  IMAGES["wB"] - p.image.load("3407_white-bishop.png")
-  IMAGES["bB"] - p.image.load("3401_black-bishop.png")
-  IMAGES["wR"] - p.image.load("3406_white-rook.png")
-  IMAGES["bR"] - p.image.load("3400_black-rook.png")
-  IMAGES["wQ"] - p.image.load("3405_white-queen.png")
-  IMAGES["bQ"] - p.image.load("3399_black-queen.png")
-  IMAGES["wK"] - p.image.load("3404_white-king.png")
-  IMAGES["bK"] - p.image.load("3398_black-king.png")
+  pieces = ["wP", "wN", "wB", "wR", "wQ", "wK", "bP", "bN", "bB", "bR", "bQ", "bK"]
   
   '''
   The main driver for our code. This will handle user input and updating the graphics.
